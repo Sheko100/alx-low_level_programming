a528b16d2d796fece5939173bd5c9f1692f2605f
Shaker Sharabi <sheko100@icloud.com>
2023-02-17 17:39:08 -0800

    Add print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in (Success)
+ */
+
+int main(void)
+{
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d++);
+	}
+
+	putchar(10);
+
+	return (0);
+}
