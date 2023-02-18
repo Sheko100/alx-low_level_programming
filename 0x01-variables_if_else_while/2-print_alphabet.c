b542a94ee05e12a7880943dc448e6ab801095fc8
Shaker Sharabi <sheko100@icloud.com>
2023-02-17 16:47:32 -0800

    Add print alphabet c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in (Success)
+ */
+
+int main(void)
+{
+	int l = 97;
+
+	while (l < 123)
+	{
+	       putchar(l++);
+	}
+	
+	putchar(10);
+
+	return (0);
+}
