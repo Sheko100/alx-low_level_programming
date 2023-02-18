68e7e70958ffe25ae6519e833ecf9870abc94193
Shaker Sharabi <sheko100@icloud.com>
2023-02-17 17:09:03 -0800

    Add print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in (Success)
+ */
+
+int main(void)
+{
+	int low_letter = 97;
+	int upper_letter = 65;
+
+	while (low_letter < 123)
+		putchar(low_letter++);
+
+	while (upper_letter < 91)
+		putchar(upper_letter++);
+
+	putchar(10);
+	
+	return (0);
+}
