8c8096862897401076f28868dfc37e633be01dec
Shaker Sharabi <sheko100@icloud.com>
2023-02-21 15:34:05 -0800

    Add add.c file and declaration

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+
+/**
+ * add - adds two integers
+ * @n1: first intger
+ * @n2: second integer
+ *
+ * Return: the result of adding the two integers
+ */
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
