56d22504522ba923a6c10b6979b2f88c9de3829d
Shaker Sharabi <sheko100@icloud.com>
2023-02-21 09:22:54 -0800

    Add abs.c file and declaration

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer which will be converted to absolute value
+ *
+ * Return: the absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	int result;
+
+	result = n - n - n;
+	return (result);
+}
