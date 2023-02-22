2df713f8a070d9a8e812136586787990830c7cdb
Shaker Sharabi <sheko100@icloud.com>
2023-02-22 10:50:18 -0800

    Add natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int n = 0;
+
+	while (n < 1024)
+	{
+		if(n % 3 == 0)
+		{
+			sum += n;
+		}
+		else if (n % 5  == 0)
+		{
+			sum += n;
+		}
+		n++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
