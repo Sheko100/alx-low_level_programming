825adc2dc73df8bf6042f064592cee75cbb84a84
Shaker Sharabi <sheko100@icloud.com>
2023-02-22 07:39:31 -0800

    Fix wrong increminting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,22 +12,26 @@ void print_times_table(int n)
 {
 	int r = n + 1, c, hundreds, tens, ones, delta = 0;
 
-	if (n < 15 && n > 0)
+	if (n < 15 && n >= 0)
 	{
 		while (r > 0)
-		{
-			hundreds = 0, tens = 0, ones = 0, c = n + 1;
+		{ hundreds = 0, tens = 0, ones = 0, c = n + 1;
 			while (c > 0)
 			{
 				if (ones > 9)
 				{
-					ones = ones % 10, tens++;
+					if (ones > 19)
+					{ tens = tens + 2; }
+					else
+					{ tens++; }
+					ones = ones % 10;
 				}
 				if (tens > 9)
-				{ tens = tens % 10, hundreds++;
-				}
+				{ tens = tens % 10, hundreds++; }
 				if (hundreds == 0)
-				{ _putchar(' ');
+				{
+					if (c != n + 1)
+					{ _putchar(' '); }
 				}
 				else
 				{ _putchar(hundreds + '0');
@@ -35,17 +39,14 @@ void print_times_table(int n)
 				if (hundreds == 0 && tens == 0)
 				{
 					if (c != n + 1)
-					{
-						_putchar(' ');
-					}
+					{ _putchar(' '); }
 				}
 				else
-				{
-					_putchar(tens + '0');
-				} _putchar(ones + '0');
+				{ _putchar(tens + '0'); }
+				_putchar(ones + '0');
 				if (c > 1)
-				{ _putchar(','), _putchar(' ');
-				} ones = ones + delta, c--;
+				{ _putchar(','), _putchar(' '); }
+				ones = ones + delta, c--;
 			} _putchar(10), delta++, r--;
 		}
 	}
