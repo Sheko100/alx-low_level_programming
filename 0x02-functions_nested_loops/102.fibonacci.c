e214134ba54d63bf0565722f9a0f21b3be854c41
Shaker Sharabi <sheko100@icloud.com>
2023-02-22 11:23:11 -0800

    Add 102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/102.fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int num1 = 0, num2 = 1, num3 = 0;
+	int i = 0;
+
+	while (i < 50)
+	{
+		num3 = num2 + num1;
+
+		printf("%d, ", num3);
+		num1 = num2;
+		num2 = num3;
+		i++;
+	}
+
+	return (0);
+}
