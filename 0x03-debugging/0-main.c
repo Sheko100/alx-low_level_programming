d1a1bb9ac19dfa873a55dac2192d34d16fa05dd9
Shaker Sharabi <sheko100@icloud.com>
2023-02-23 06:48:59 -0800

    Add 0-main.c testing file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests positive_or_negative function if it prints zero
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int zero = 0;
+
+	positive_or_negative(zero);
+
+	return (0);
+}
