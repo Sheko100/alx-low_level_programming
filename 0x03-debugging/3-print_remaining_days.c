844f6715b4beaaa8123e7828b12b121498737a2d
Shaker Sharabi <sheko100@icloud.com>
2023-02-23 13:31:32 -0800

    Fix number of days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,14 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-			day++;
+			if(month == 2)
+			{
+				day--;
+			}
+			else
+			{
+				day++;
+			}
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -31,7 +38,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 
 	}
