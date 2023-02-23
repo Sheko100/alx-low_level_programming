bab53c83e39ab281613ef0938768731dbee7c5fd
Shaker Sharabi <sheko100@icloud.com>
2023-02-23 08:40:12 -0800

    Fix equal values case

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,42 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a == b || b == c)
 	{
-		largest = a;
+		if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
 	}
-	else if (c > b && c > a)
+	else if (a == c)
 	{
-		largest = c;
+		if (a > b)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = b;
+		}
 	}
 	else
 	{
-		largest = b;
+		if (a > b && a > c)
+		{
+			largest = a;
+		}
+		else if (c > b && c > a)
+		{
+			largest = c;
+		}
+		else
+		{
+			largest = b;
+		}
 	}
 
 	return (largest);
