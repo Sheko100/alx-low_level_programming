84415c9c5e8e7d405ad6288a96e242165cdc6672
Shaker Sharabi <sheko100@icloud.com>
2023-02-23 17:29:32 -0800

    Add isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if the character is digit
+ * @c: character to check
+ *
+ * Return: 1 if digit
+ * 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
