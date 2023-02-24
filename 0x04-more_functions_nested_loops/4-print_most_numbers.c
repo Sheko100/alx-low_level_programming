b2e5046f38c08d8af9d922f12fe0b1eca80d7f73
Shaker Sharabi <sheko100@icloud.com>
2023-02-23 18:38:43 -0800

    Add print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers()
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if ( i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar(10);
+}
