27fe558baea30983ec2822d3d7703b4526d6cb7c
Shaker Sharabi <sheko100@icloud.com>
2023-02-23 18:40:24 -0800

    Fix coding style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,13 @@
  * Return: void
  */
 
-void print_most_numbers()
+void print_most_numbers(void)
 {
 	int i = 48;
 
 	while (i < 58)
 	{
-		if ( i != 50 && i != 52)
+		if (i != 50 && i != 52)
 		{
 			_putchar(i);
 		}
