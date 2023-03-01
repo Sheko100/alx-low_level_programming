5e817ba8e37f9b4befdc5d2b39c6119f3c71ad4c
Shaker Sharabi <sheko100@icloud.com>
2023-02-28 18:09:41 -0800

    Add print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: array of integers
+ * @n: number of array elements
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int *ap;
+	int i = 0;
+
+	ap = a;
+
+	while (i < n)
+	{
+		printf("%d, ", *(ap + i));
+		i++;
+	}
+	printf("\n");
+}
