d2fef2ee3de6f5dae159ea937b83605f24584209
Shaker Sharabi <sheko100@icloud.com>
2023-03-01 17:56:05 -0800

    Add rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: array number of elements
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *ap;
+	int *revap;
+	int i = 0;
+	int reva[200];
+
+	ap = a;
+	revap = reva;
+
+	while (n > 0)
+	{
+		*(revap + i) = *(ap + (n - 1));
+		i++;
+		n--;
+	}
+	while (n < i)
+	{
+		*(ap + n) = *(revap + n);
+		n++;
+	}
+}
