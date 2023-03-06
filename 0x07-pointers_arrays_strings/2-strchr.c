4f88ad5a013a091f390ade544a99506e83d25aca
Shaker Sharabi <sheko100@icloud.com>
2023-03-06 07:09:38 -0800

    Fix null terminator case

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-
+	
 	while (*(s + i))
 	{
 		if (*(s + i) == c)
@@ -18,6 +18,11 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 		i++;
+	}
+	
+	if (c == '\0')
+	{
+		return (s + i + 1);
 	}
 	return (NULL);
 }
