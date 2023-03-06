074e56ffd01cd04356ff8bbbbf0234bd47d6ca5a
Shaker Sharabi <sheko100@icloud.com>
2023-03-06 07:11:08 -0800

    Fix coding style

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	
+
 	while (*(s + i))
 	{
 		if (*(s + i) == c)
@@ -19,7 +19,7 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	
+
 	if (c == '\0')
 	{
 		return (s + i + 1);
