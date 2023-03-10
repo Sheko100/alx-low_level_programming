590ca186bd468b3568d5bf37d2dbc6cccc449a2b
Shaker Sharabi <sheko100@icloud.com>
2023-03-10 09:18:41 -0800

    Fix coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,14 @@
 
 /**
  * main - prints the program name
+ * @argc: arguments count
+ * @argv: array of arguments strings
  *
  * Return: void
  */
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);	
+	printf("%s\n", argv[0]);
 	return (0);
 }
