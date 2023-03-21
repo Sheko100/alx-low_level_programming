460bff828e3a26b2ba715d1beb382565316ec146
Shaker Sharabi <sheko100@icloud.com>
2023-03-20 18:55:29 -0700

    Add init_dog prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,11 @@
  *
  * Description: Characteristic for any dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
