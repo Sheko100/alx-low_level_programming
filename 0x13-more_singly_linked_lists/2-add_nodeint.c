927b7ee8a2fd632357c0d7ea8ef732bf1b40148c
Shaker Sharabi <sheko100@icloud.com>
2023-04-03 23:43:04 -0700

    Fix coding style

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,34 +5,31 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to pointer pointing at the head
+ * @n: int to store in node
  *
- * Return: address of the new element 
+ * Return: address of the new element
  * NULL on failing
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node;
-	listint_t *tmp;
 
-	if ((node = malloc(sizeof(listint_t))))
-	{
-		if (head)
-		{
-			tmp = *head;
-			node->next = tmp;
-		}
-		else
-		{
-			node->next = NULL;
-		}
-		node->n = n;
-		*head = node;
-	}
-	else
+	node = malloc(sizeof(listint_t));
+
+	if (node == NULL)
 	{
 		return (NULL);
 	}
-	
+	if (head)
+	{
+		node->next = *head;
+	}
+	else
+	{
+		node->next = NULL;
+	}
+	node->n = n;
+	*head = node;
 	return (node);
 }
