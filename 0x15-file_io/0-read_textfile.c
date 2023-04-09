8f6f0925750449293727e8147bbb28935a030327
Shaker Sharab <sheko100@icloud.com>
2023-04-09 12:32:21 +0000

    Add read_textfile.c file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output
+ * @filename: file name to read
+ * @letters: number of letters
+ *
+ * Return: number of letters 
+ * 0 if the file can't be opened or
+ * the filename is null or
+ * write function fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n;
+	char buf[10000];
+
+	fd = open(filename, O_RDWR, 0);
+
+	if (fd < 0)
+		return (0);
+
+	if (read(fd, buf, letters) <= 0)
+		return (0);
+	/*
+	 * Check all the cases
+	 */
+
+	n = write(STDOUT_FILENO, buf, letters);
+
+	if (n <= 0)
+		return (0);
+
+	close(fd);
+
+	return n;	
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_h
+#define MAIN_h
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
