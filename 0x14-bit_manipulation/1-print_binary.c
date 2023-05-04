ed9e6be0085d9e4ca30b0a35d7f0276a72990f08
Shaker Sharabi <sheko100@icloud.com>
2023-05-04 10:55:56 -0700

    Add print_binary.c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * remain - gets the remained number after reducing by 2
+ * @n: number to get the remainder
+ *
+ * Return: remainder of n
+ */
+
+int remain(unsigned long int n)
+{
+	if (n == 1)
+		return (1);
+
+	while (n > 2)
+	{
+		n = n - 2;
+		if (n == 1)
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * dividebytwo - divide a number by 2
+ * @n: number to divide by 2
+ *
+ * Return: division result
+ */
+
+int dividebytwo(unsigned long int n)
+{
+	int i = 0;
+
+	while (n > 1)
+	{
+		n -= 2;
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * print_binary - converts decimal to binary and prints it
+ * @n: decimal number to convert
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int rem;
+	char zero = 48;
+	char one = 49;
+
+	if (n == 0)
+	{
+		return;
+	}
+
+	rem = remain(n);
+	n = dividebytwo(n);
+	print_binary(n);
+	if (rem == 0)
+		write(1, &zero, 1);
+	else if (rem == 1)
+		write(1, &one, 1);
+}
