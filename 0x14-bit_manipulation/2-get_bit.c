9e13fbacb3efff5593de2b8c0583e5f8408f8291
Shaker Sharabi <sheko100@icloud.com>
2023-05-04 15:56:44 -0700

    Add get_bit.c file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * deci_to_bin - converts decimal to binary string
+ * @n: decimal number to convert
+ *
+ * Return: pointer to binary string
+ */
+
+char *deci_to_bin(unsigned long int n)
+{
+	int rem = 0;
+	char bin[32];
+	char *binp;
+	int i = 0;
+
+	binp = bin;
+
+	while (n > 0)
+	{
+		rem = n % 2;
+		if (rem == 0)
+			bin[i] = '0';
+		else if (rem == 1)
+			bin[i] = '1';
+
+		n = n / 2;
+		i++;
+	}
+	bin[i] = '\0';
+
+	return (binp);
+}
+
+/**
+ * get_bit - gets bit value in specific index
+ * @n: number to look into
+ * @index: bit index
+ *
+ * Return: bit value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+	char *bin;
+
+	bin = deci_to_bin(n);
+
+	if (bin == NULL)
+		return (-1);
+
+	if (bin[index] == '0')
+		bit = 0;
+	else if (bin[index] == '1')
+		bit = 1;
+
+	return (bit);
+}
