49ed57be87b1f5e67a96c40818268ebe5bcfb725
Shaker Sharabi <sheko100@icloud.com>
2023-05-04 18:31:14 -0700

    Fix coding style

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/** clear_bit - sets the value of a bit to 0 at a given index
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: number to look into
  * @index: bit index to modify
  *
@@ -17,7 +18,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (*n == 0)
 		return (1);
-	
+
 	*n ^= 1UL << index;
 	return (1);
 }
