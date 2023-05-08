845ce4b1f42d20c46f123ab0dc31ecbd183f9d2e
Shaker Sharabi <sheko100@icloud.com>
2023-05-08 10:49:03 -0700

    Fix NULL case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * isnull - checks string if its NULL
+ * @str: pointer to str
+ *
+ * Return: 1 is the string is NULL
+ * 0 if the string is not NULL
+ */
+
+int isnull(const char *str)
+{
+	int isn, isu, isl, isend;
+
+	isn = str[0] == 'N';
+	isu = str[1] == 'U';
+	isl = str[2] == 'L' && str[3] == 'L';
+	isend = str[4] == '\0';
+
+	if (isn && isu && isl && isend)
+		return (1);
+
+	return (0);
+}
+
+/**
  * read_textfile - reads file and write content to the standard output
  * @filename: path to the file
  * @letters: letters count to read
@@ -14,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, readlen, writelen;
 	char buf[1024];
 
-	if (filename == NULL)
+	if (isnull(filename))
 		return (0);
 
 	fd = open(filename, O_RDONLY);
