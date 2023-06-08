aefb39ca5e8952cd301684b50ea7d7471350e8fe
Shaker Sharabi <sheko100@icloud.com>
2023-06-07 19:00:51 -0700

    Add is_palindrome.c file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if string is palindrome
+ * @s: pointer to string
+ *
+ * Return: 1 if a string is a palindrome
+ * 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,3 @@
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
