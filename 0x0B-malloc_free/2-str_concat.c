4d33c2012770500e914f039bd4fa82626d5891f3
Shaker Sharabi <sheko100@icloud.com>
2023-06-12 00:11:36 -0700

    Fix NULL arguments

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,16 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int  i = 0, len1 = 0, len2 = 0;
 
-	while (s1[len1])
-		len1++;
-
-	while (s2[len2])
-		len2++;
+	if (s1 != NULL)
+	{
+		while (s1[len1])
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[len2])
+			len2++;
+	}
 
 	str = malloc((len1 + len2) * sizeof(char) + 1);
 	if (str == NULL)
