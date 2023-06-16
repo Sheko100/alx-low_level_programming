f65786389f29b882237b4dc63af9e651e28de823
Shaker Sharabi <sheko100@icloud.com>
2023-06-15 19:46:13 -0700

    Add mul.c file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * err - prints error and exits with 98
+ *
+ * Return: void
+ */
+
+void err(void)
+{
+	char errstr[] = "Error";
+	int i = 0;
+
+	while (errstr[i])
+	{
+		_putchar(errstr[i]);
+		i++;
+	}
+	_putchar('\n');
+	exit(98);
+}
+
+/**
+ * isnum - check is the string is number
+ * @s: string to check
+ *
+ * Return: 1 if it's number
+ * 0 if not
+ */
+
+int isnum(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			printf("%c\n", s[i]);
+			return (0);
+		}
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * printnum - prints number string
+ * @str: string of numbers
+ *
+ * Return: void always
+ */
+
+void printnum(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @ac: arguments count
+ * @av: arguments values
+ *
+ * Return: void always
+ */
+
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac != 3)
+	{
+		err();
+	}
+	if (isnum(av[1]) && isnum(av[2]))
+	{
+		res = atoi(av[1]) * atoi(av[2]);
+	}
+	else
+		err();
+
+	printf("%d\n", res);
+	return (0);
+}
