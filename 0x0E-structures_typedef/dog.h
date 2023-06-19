30ef2b2df5a3bba3df4b5808173a499da3abbf98
Shaker Sharabi <sheko100@icloud.com>
2023-06-19 06:21:36 -0700

    Add dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: dog characteristics
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
