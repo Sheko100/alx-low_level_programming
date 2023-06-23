7b6cb15e030b53e42966dbc0b3be3424291a5619
Shaker Sharabi <sheko100@icloud.com>
2023-06-22 19:01:28 -0700

    Add struct format

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,16 @@
 #define VAR_FUNC_H
 #include <stdarg.h>
 
+/** struct format - Struct for format specifier
+ * @char: specifier
+ * @fp: data type pointer
+ */
+typedef struct format
+{
+	char spc;
+	void (*fp)(void *);
+} frmt;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
