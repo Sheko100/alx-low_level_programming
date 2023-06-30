769a8546631d7ff917fc6d863a25eda2588688a9
Shaker Sharabi <sheko100@icloud.com>
2023-06-30 07:18:10 -0700

    Add free_list.c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the list head node
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *np;
+	list_t *temp;
+
+	np = head;
+	while (np->next)
+	{
+		temp = np->next;
+		free(np->str);
+		free(np);
+		np = temp;
+	}
+	free(np->str);
+	free(np);
+}
