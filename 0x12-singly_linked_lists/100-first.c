cd4ac5934444c04879998b943c78f2dbd246e566
Shaker Sharabi <sheko100@icloud.com>
2023-06-30 11:24:43 -0700

    Add first.c file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void first(void) __attribute__((constructor));
+
+/**
+ * first - prints a sentence before the main function executes
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
+
