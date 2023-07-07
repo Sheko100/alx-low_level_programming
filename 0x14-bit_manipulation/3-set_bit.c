ff5a9c7980da67f854559fca6a26d2ccbdbdbd65
Shaker Sharabi <sheko100@icloud.com>
2023-07-06 17:46:18 -0700

    Add set_bit.c file

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: number to look into
+ * @index: bit index to modify
+ *
+ * Return: 1 on success
+ * -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+	*n = *n >> index;
+	*n += 1;
+	*n = *n << index;
+	return (1);
+}
