54193e00cba9c6334b1621be23e43fba7cc00ca6
Shaker Sharabi <sheko100@icloud.com>
2023-07-10 09:23:21 -0700

    Add read_textfile.c file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters: number of letters to read
+ *
+ * Return: number of letters actually read
+ * 0 if the file can't be opened of read or filename is NULL
+ * or if write fails or didn't write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t readbytes, writebytes;
+	int fd;
+	char buf[1024];
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	readbytes = read(fd, buf, letters);
+	if (readbytes == -1)
+		return (0);
+
+	writebytes = write(STDOUT_FILENO, buf, readbytes);
+	if (writebytes == -1 || writebytes != readbytes)
+		return (0);
+
+	return (writebytes);
+}
