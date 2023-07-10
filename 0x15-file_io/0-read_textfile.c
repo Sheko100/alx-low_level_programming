5692df9ccc63a1abb6d7304c92f7095db335fe0f
Shaker Sharabi <sheko100@icloud.com>
2023-07-10 09:28:23 -0700

    Fix NULL case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,6 +16,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char buf[1024];
 
+	if (filename == NULL || letters == 0)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
