e3f2fe77024271dee6f7a8d479a748d6a1c3c6a9
Shaker Sharabi <sheko100@icloud.com>
2023-08-18 03:58:49 +0300

    Fix first index case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	nodep = *h;
 	while (nodep != NULL)
 	{
-		if (idx - i == 1)
+		if (idx == i)
 		{
+			if (idx == 0)
+			{
+				newnode = add_dnodeint(h, n);
+				break;
+			}
 			newnode = malloc(sizeof(dlistint_t));
 			if (newnode == NULL)
 				return (NULL);
