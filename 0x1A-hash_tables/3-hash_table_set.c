33007afb47f385d0a160b1823c473f47c778f888
Shaker Sharabi <sheko100@icloud.com>
2023-09-08 23:01:15 +0300

    Add 3-hash_table_set.c file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: an array of hash table
+ * @key: the key name
+ * @value: the value to set
+ *
+ * Return: 1 on success
+ * 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int hash, index, size;
+	char *valuecpy = NULL, *keycpy = NULL;
+	hash_node_t **array = NULL, *hashnode = NULL, *temp = NULL;
+
+	size = ht->size;
+	array = ht->array;
+
+	if (key == NULL || value == NULL)
+		return (0);
+	else if (strlen(key) < 1)
+		return (0);
+
+	keycpy = malloc(strlen(key) + 1);
+	if (keycpy == NULL)
+		return (0);
+	keycpy = strcpy(keycpy, key);
+
+	valuecpy = malloc(strlen(value) + 1);
+	if (valuecpy == NULL)
+		return (0);
+	valuecpy = strcpy(valuecpy, value);
+
+	hashnode = malloc(sizeof(hash_node_t));
+	if (hashnode == NULL)
+		return (0);
+	hashnode->key = keycpy;
+	hashnode->value = valuecpy;
+	hashnode->next = NULL;
+
+	index = key_index(key, size);
+	if (array[index] != NULL)
+		hashnode->next = array[index];
+
+	array[index] = hashnode;
+
+	return (1);
+}
