a3703404611c9efe1c2a4da81f376b472c0ae2fd
Shaker Sharabi <sheko100@icloud.com>
2023-12-12 12:29:20 +0200

    Add search_algos.h file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+#include <stdio.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
