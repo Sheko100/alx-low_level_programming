c942d65a0db546e61a991b8f08b4dd2c5959fafa
Shaker Sharabi <sheko100@icloud.com>
2023-12-13 21:31:39 +0200

    Add 100-jump.c file

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,6 +29,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	int i = 0;
 	size_t block = sqrt(size);
+
 	if (array == NULL)
 		return (-1);
 
@@ -52,6 +53,7 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array[i] == value)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		return (i);
 	}
