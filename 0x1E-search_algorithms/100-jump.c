30b90487f3c63ecbebae7fdacdad2c1cd370ef6c
Shaker Sharabi <sheko100@icloud.com>
2023-12-13 21:15:59 +0200

    Add 100-jump.c file

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * min - gets the minimum of two values
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: the minimum number
+ */
+int min(int num1, int num2)
+{
+	int min = num1 < num2 ? num1 : num2;
+
+	return (min);
+}
+/**
+ * jump_search - searches for a value in a sorted array of integers using
+ * the Jump search algorithm
+ * @array: pointer to the array first element
+ * @size: array size
+ * @value: value to search for
+ *
+ * Return: the value index on success
+ * -1 if the array is null or the value is not found
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int i = 0;
+	size_t block = sqrt(size);
+	if (array == NULL)
+		return (-1);
+
+	while (array[min(block, size) - 1] < value)
+	{
+		i = block;
+		block += sqrt(size);
+		if (i >= size)
+			return (-1);
+	}
+
+	while (array[i] < value)
+	{
+		i++;
+		if (i == min(block, size))
+			return (-1);
+	}
+
+	if (array[i] == value)
+		return (i);
+
+	return (-1);
+}
