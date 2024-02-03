a868b200a084cd5dee8e183491be74a66c59c56d
Shaker Sharabi <sheko100@icloud.com>
2024-02-03 19:59:51 +0200

    rollback the repo

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEARCH_H
-#define SEARCH_H
-#include <stdio.h>
-#include <stddef.h>
-
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-#endif
